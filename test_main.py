153dbd43e847b0639ed56771a7d16f44d5357c45
Scott Breitenbach <scottbreitenbach@Scotts-Air.wowway.com>
2020-08-25 21:20:57 -0400

    clean up of variables and adding windows support

--- test_main.py
+++ test_main.py
@@ -1,14 +1,17 @@
 import main
+
+
 def test_AlwaysPasses():
-    assert(1==1)
+    assert(1 == 1)
+
 
 def test_Pinging_Loopback_Succeeds():
     IP = "127.0.0.1"
-    result = main.pingTheHost(IP)
-    assert(result=="Success")
+    result = main.ping_the_host(IP)
+    assert(result == "Success")
+
 
 def test_Pinging_Unused_IP_Fails():
     IP = "198.51.100.1"
-    result = main.pingTheHost(IP)
-    assert(result=="Failure")
-    
+    result = main.ping_the_host(IP)
+    assert(result == "Failure")
